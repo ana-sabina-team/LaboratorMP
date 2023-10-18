176e978a4d9be07a7e60849b968ad18479092744
Ana Rus <117738169+anarus7@users.noreply.github.com>
2023-10-18 12:45:44 +0300

    -updated

--- src/main/src/ro/ntt/catalog/repository/BookRepository.java
+++ src/main/src/ro/ntt/catalog/repository/BookRepository.java
@@ -1,21 +1,32 @@
 package ro.ntt.catalog.repository;
+import ro.ntt.catalog.model.BaseEntity;
 import ro.ntt.catalog.model.Book;
 
+import javax.xml.validation.Validator;
 import java.util.Iterator;
+import java.util.Optional;
 
 
-public interface BookRepository {
+public interface BookRepository<ID,T extends BaseEntity<ID>> {
 
     Book fiindPme(Long id) ;
 
     Iterator<Book> findOne();
+
+
     Iterable<Book> findAll();
 
-    //iterable,interator -interfete , poti utiliza metoda din collection
 
-    Book save(Book entity);
+
+
+    Optional<T> save(T entity) ;
+
 
     Book delete(Long id);
 
-    Book update(Book entity) throws Exception;
+
+   Optional<T> delete (ID id);
+
+
+   Optional<T> update (T entity) ;
 }
---
+++ src/main/src/ro/ntt/catalog/repository/InMemoryRepository.java
@@ -0,0 +1,4 @@
+package ro.ntt.catalog.repository;
+
+public class InMemoryRepository {
+}
