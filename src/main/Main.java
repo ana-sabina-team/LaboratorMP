3e97bea5444cb72fedbbf6c9a2788cc4bb95c5de
Sabina <hsabinamaria@gmail.com>
2023-10-19 12:48:49 +0300

    Retry

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,20 @@
 package main;
 
 import main.UI.Console;
+import main.repository.BookRepository;
 import main.repository.ClientRepository;
 import main.repository.ClientRepositoryImpl;
+import main.repository.BookRepositoryImpl;
+import main.service.BookService;
 import main.service.ClientService;
 
 public class Main {
     public static void main(String[] args) {
-
+        BookRepository bookRepository = new BookRepositoryImpl();
+        BookService bookService = new BookService(bookRepository);
         ClientRepository clientRepository = new ClientRepositoryImpl();
         ClientService clientService = new ClientService(clientRepository);
-        Console console = new Console(clientService);
+        Console console = new Console(clientService,bookService );
 
         console.runMenu();
 
